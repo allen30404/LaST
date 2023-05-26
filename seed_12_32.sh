75947c9307f16762573a872cb25157f1e95c23cc
allen30404 <allen30404@icloud.com>
2023-05-26 18:09:38 +0800

    實驗結果上傳

--- seed_12_32.sh
+++
@@ -1,4 +0,0 @@
-for i in {12..32}
-do
-    CUDA_VISIBLE_DEVICES=2 python run.py --data ETTh1 --features S --seq_len 201 --pred_len 720 --latent_size 128 --batch_size 32 --patience 10 --Encoder_Muti_Scale --Mean_Var_Model Conv --seed $i
-done
---
+++ seed_96_116_168.sh
@@ -0,0 +1,4 @@
+for i in {96..116}
+do
+    CUDA_VISIBLE_DEVICES=3 python run.py --data Weather --features M --seq_len 201 --pred_len 168 --latent_size 128 --batch_size 32 --patience 10 --Encoder_Muti_Scale --Mean_Var_Model mlp --seed $i
+done
